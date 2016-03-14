66d1bddd2c7f800e1698e719869c4be9645f0ea3
OceanOSdev <ThomasTNF@live.com>
2016-03-13 22:17:11 -0500

    Proof of concept, kinda not really

---
+++ blehAgain/Models/HealthModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace blehAgain.Models
+{
+    public class HealthModel
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public decimal Height { get; set; }
+        public decimal Weight { get; set; }
+        
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+
+        public decimal BMI { get; set; }
+    }
+}
